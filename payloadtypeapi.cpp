4a148e717038674418508a3ef043070d4b258585
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2012-05-02 11:16:12 +0200

    Disable user data usage with payloadtype

--- payloadtypeapi.cpp
+++ payloadtypeapi.cpp
@@ -4,7 +4,7 @@
 PayloadTypeAPI::PayloadTypeAPI(const boost::shared_ptr<CoreAPI> &core, PayloadType *payloadType) :
 		mCore(core), mPayloadType(payloadType) {
 	FBLOG_DEBUG("PayloadTypeAPI::PayloadTypeAPI", this);
-	mPayloadType->user_data = this;
+	//mPayloadType->user_data = this;
 
 	registerProperty("type", make_property(this, &PayloadTypeAPI::getType));
 	registerProperty("clockRate", make_property(this, &PayloadTypeAPI::getClockRate));
@@ -43,12 +43,12 @@ PayloadTypeAPI::~PayloadTypeAPI() {
 }
 
 boost::shared_ptr<PayloadTypeAPI> PayloadTypeAPI::get(const boost::shared_ptr<CoreAPI> &core, PayloadType *payloadType) {
-	void *ptr = payloadType->user_data;
+	//void *ptr = payloadType->user_data;
 	boost::shared_ptr<PayloadTypeAPI> shared_ptr;
-	if (ptr == NULL) {
+	/*if (ptr == NULL) {*/
 		shared_ptr = boost::shared_ptr<PayloadTypeAPI>(new PayloadTypeAPI(core, payloadType));
-	} else {
+	/*} else {
 		shared_ptr = boost::static_pointer_cast<PayloadTypeAPI>(reinterpret_cast<PayloadTypeAPI *>(ptr)->shared_from_this());
-	}
+	}*/
 	return shared_ptr;
 }
