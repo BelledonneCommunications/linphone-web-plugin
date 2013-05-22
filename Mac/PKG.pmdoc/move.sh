90b0daa2b3a6eb91047f2609342cb1f4289ff5eb
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-05-22 17:00:26 +0200

    Fix Mac pkg installer

--- Mac/PKG.pmdoc/move.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf ${HOME}/Library/Internet\ Plug-Ins/@FBSTRING_PluginFileName@.@PLUGIN_EXT@
-mv /tmp/@FBSTRING_PluginFileName@.@PLUGIN_EXT@ ${HOME}/Library/Internet\ Plug-Ins/
-exit 0
