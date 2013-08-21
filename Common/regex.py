66163fa838c6ce75405683cf7f87a4e1ddac83a6
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-08-21 16:54:30 +0200

    Add conditionnal blocks system in cmake configured files

---
+++ Common/regex.py
@@ -0,0 +1,25 @@
+import re, sys
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
+
+def replace(src, dest, variable, value):
+  infile = open(src, "r")
+  intext = infile.read()
+  infile.close()
+  match = r"\${IF " + variable + r"}(.*?)\${ENDIF}"
+  if str2bool(value):
+    replace = r"\1"
+  else:
+    replace = r""
+  re_match = re.compile(match, re.MULTILINE|re.DOTALL)
+  outtext = re.sub(re_match, replace, intext)
+  outfile = open(dest, "w")
+  outfile.write(outtext)
+  outfile.close() 
+
+if __name__ == '__main__':
+  if len(sys.argv) != 5:
+    sys.exit(5)
+  replace(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    
