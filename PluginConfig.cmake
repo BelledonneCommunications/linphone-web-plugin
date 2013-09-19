fa9bd36f8ea797f2cdb837b373e04bbda6df27e4
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-09-19 16:32:27 +0200

    Update plugin version to 0.0.2.5.

--- PluginConfig.cmake
+++ PluginConfig.cmake
@@ -48,8 +48,8 @@ set(FBSTRING_CompanyName "Belledonne Communications")
 # Company id
 set(FBSTRING_CompanyDomain "com.belledonne-communication")
 
-set(FBSTRING_PLUGIN_VERSION "0.0.2.4")
-set(FBSTRING_PLUGIN_VERSION_ACTIVEX "0,0,2,4")
+set(FBSTRING_PLUGIN_VERSION "0.0.2.5")
+set(FBSTRING_PLUGIN_VERSION_ACTIVEX "0,0,2,5")
 set(FBSTRING_LegalCopyright "Copyright 2013 Belledonne Communications")
 set(FBSTRING_PluginFileName "np${PLUGIN_NAME}")
 
