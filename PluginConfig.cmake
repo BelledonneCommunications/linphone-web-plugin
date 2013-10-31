7f33bb02404aefc64dab897c764cfae1e66f99bb
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-10-31 16:46:08 +0100

    Update core rootfs version.

--- PluginConfig.cmake
+++ PluginConfig.cmake
@@ -22,7 +22,7 @@ set(PLUGIN_NAME "linphone-web")
 set(PLUGIN_PREFIX "LINWEB")
 set(COMPANY_NAME "belledonne-communications")
 set(PLUGIN_SHAREDIR "linphoneweb")
-set(CORE_ROOTFS_VERSION "master-b21f3c293abe87e97d5128dcdb9555e35ac839e1")
+set(CORE_ROOTFS_VERSION "master-eec4eca2c69aeb5cd0addeded6bc2969f821f8cc")
 set(CORE_ROOTFS_URL "http://www.linphone.org/snapshots/linphone-web/rootfs/linphone-web-rootfs")
 
 # ActiveX constants:
