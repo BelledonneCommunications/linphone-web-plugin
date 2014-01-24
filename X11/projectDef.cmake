b8228fe23fdac22be6c6e325fa9b987fc3fb9087
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-01-24 09:29:10 +0100

    Fix wrong copyright information.

--- X11/projectDef.cmake
+++ X11/projectDef.cmake
@@ -1,6 +1,6 @@
 ##
 # Linphone Web - Web plugin of Linphone an audio/video SIP phone
-# Copyright (C) 2012  Yann Diorcet <yann.diorcet@linphone.org>
+# Copyright (C) 2012-2014 Belledonne Communications
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
